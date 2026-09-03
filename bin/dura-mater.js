50a7d4612721c3cb64d73406beb305a38adcf603
Victor Gardrinier <v.gardrinier@gmail.com>
2026-09-03 14:15:01 +0200

    Add bin/dura-mater.js

---
+++ bin/dura-mater.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { run } from "../src/cli.js";
+run(process.argv.slice(2)).catch((error) => {
+  console.error(`Dura Mater could not install: ${error.message}`);
+  process.exitCode = 1;
+});
